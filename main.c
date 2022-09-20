23a34b3f3d301aab8fffc1798b5c259ecadb39a6
Ma7m0ud3id127 <mahmoudeid127127@gmail.com>
2022-09-20 22:37:29 +0200

    sconde

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 20, 2022
+ *      Author: now
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	for(int i =0;i<6;i++){
+		printf("hello ahmed \n");
+	}
+
+	//printf("hello Mahmoud \n");
+	//printf("hello Omar \n");/* prints !!!Hello World!!! */
+	return 0;
+}
+
+
